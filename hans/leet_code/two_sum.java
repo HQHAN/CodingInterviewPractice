d1597fde289fbd4f2e80b947adaa0240aa4062b3
HyunQ, Han <hyunqh@gmail.com>
2021-03-29 11:40:27 +0900

    Improved

--- hans/leet_code/two_sum.java
+++ hans/leet_code/two_sum.java
@@ -1,18 +1,30 @@
 import java.io.*;
 
 class Solution {
+//    public int[] twoSum(int[] nums, int target) {
+//        int[] solution = new int[2];
+//        for(int i=0; i<nums.length; i++) {
+//            int complement = target - nums[i];
+//            for(int j=i+1; j<nums.length; j++) {
+//                if(complement == nums[j]) {
+//                    solution[0] = i;
+//                    solution[1] = j;
+//                    return solution;
+//                }
+//            }
+//        }
+//        return solution;
+//    }
     public int[] twoSum(int[] nums, int target) {
-        int[] solution = new int[2];
+        Map<Integer, Integer> tracker = new HashMap<Integer, Integer>();
         for(int i=0; i<nums.length; i++) {
             int complement = target - nums[i];
-            for(int j=i+1; j<nums.length; j++) {
-                if(complement == nums[j]) {
-                    solution[0] = i;
-                    solution[1] = j;
-                    return solution;
-                }
+            if(tracker.containsKey(complement) && tracker.get(complement) != i) {
+                return new int[] {i, tracker.get(complement)};
             }
+            tracker.put(nums[i] , i);
         }
-        return solution;
+
+        throw new IllegalArgumentException("No two sum solution");
     }
 }
